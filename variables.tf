446318a5403ec571abe60e5ab9a07e02c75ddf92
Kanav Gathe <80106966+SlayerK15@users.noreply.github.com>
2024-10-02 16:15:24 +0530

    Set up CI/CD pipeline with GitHub Actions and Terraform

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ecs_cluster_name" {
+  description = "ECS Cluster Name"
+  type        = string
+  default     = "medusa-cluster"
+}
+
+variable "ecs_service_name" {
+  description = "ECS Service Name"
+  type        = string
+  default     = "medusa-service"
+}
+
+variable "image_uri" {
+  description = "The URI of the Docker image to deploy"
+  type        = string
+}
+
+# Data source for Availability Zones
+data "aws_availability_zones" "available" {}
